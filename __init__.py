8d9284c26ffa3397fa71f56fb6d3168161ce9113
Marcel Livotto <freelance@livottomx.com>
2024-02-17 08:00:54 +0100

    Add pylint

---
+++ __init__.py
@@ -0,0 +1,4 @@
+# pylint: disable=C0103
+"""daqhats mock library for local development"""
+
+from src.mcc118 import mcc118
---
+++ src/mcc118.py
@@ -0,0 +1,6 @@
+"""Mocks for MCC118 boards"""
+
+class mcc118:
+    """MCC118 board handler"""
+    def __init__(self) -> None:
+        pass
