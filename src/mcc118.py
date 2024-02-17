c277c3f1fdce93682da96e8846ee3038a353e674
Marcel Livotto <freelance@livottomx.com>
2024-02-17 08:21:19 +0100

    Add dummy methods to mcc118 mock class

--- src/mcc118.py
+++ src/mcc118.py
@@ -2,5 +2,25 @@
 
 class mcc118:
     """MCC118 board handler"""
-    def __init__(self) -> None:
-        pass
+    def __init__(self, address: int) -> None:
+        self._address = address
+
+    def a_in_scan_actual_rate(self, num_channels: int, scan_rate: int) -> int:
+        """Calculate and return the actual scan rate"""
+        raise NotImplementedError()
+
+    def a_in_scan_start(self, channel_mask: str, a: int, scan_rate: int, options: OptionFlags) -> None:
+        """Start a scan with the given configuration"""
+        raise NotImplementedError()
+
+    def a_in_scan_cleanup(self) -> None:
+        """Cleanup the scan and free the buffer"""
+        raise NotImplementedError()
+
+    def a_in_scan_stop(self) -> None:
+        """Stop the scan"""
+        raise NotImplementedError()
+
+    def a_in_scan_read(self, a: int, b: int) -> None:
+        """Read a batch of samples from the current scan"""
+        raise NotImplementedError()
